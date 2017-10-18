3b2c4717549f7248fe96c9560c490ec62740e60c
Smehta <mehta.sahil1997@gmail.com>
2017-10-18 20:20:04 +0200

    oana edits

--- queries.sql
+++ queries.sql
@@ -37,6 +37,22 @@ FROM accomodation
 JOIN resort
 ON accomodation.ac_ID = hostel.ac_ID;
 
-SELECT *
-FROM client c, booking b
-WHERE c.client_ID = b.booking_ID
+/*select all bookings for a client*/
+SELECT a.name,b.num_people,b.arrival_date,b.departure_date
+FROM client c, booking b, accomodation a, reserves r 
+WHERE c.client_ID = r.client_ID and b.booking_ID = r.booking_ID and a.ac_ID =r.ac_ID 
+
+/*select all accomodations which have a sauna*/
+SELECT a.name, a.stars, a.description
+FROM accomodation a,facilities f, spa s
+WHERE f.ac_ID=a.ac_ID and f.f_ID=s.f_ID and s.sauna is true
+
+/*select cancelation period for a certain hotel that client has a booking with*/
+SELECT p.cancellation_period
+FROM policy p, client c, reserves r
+WHERE r.client_ID=c.client_ID and p.ac_ID=r.ac_ID
+
+/*select all active bookings for a client returning the name of the accomodation, the number of people and the stay period */
+SELECT a.name, b.arrival_date, b.departure_date, b.num_people
+FROM accomodation a, booking b, reserves r, client c
+WHERE c.client_ID=r.client_ID and b.booking_ID=r.booking_ID and a.ac_ID=r.ac_ID and b.status is true
